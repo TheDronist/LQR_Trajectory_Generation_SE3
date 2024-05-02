427ac85b514135daa5c17a00660b9a0b9f844802
TheDronist <168654473+TheDronist@users.noreply.github.com>
2024-05-02 22:00:19 +0200

    Update Rodrigues.m

--- Rodrigues.m
+++ Rodrigues.m
@@ -1,5 +1,5 @@
-function R = Rodrigues(w)
+function R=Rodrigues(w)
     if norm(w)==0, R=eye(3,3);
-        else R=eye(3,3)+sin(norm(w))*hat(w)/norm(w)+(1-cos(norm(w)))*hat(w)*hat(w)/(norm(w)^2);
+        else R=eye(3,3)+sin(norm(w))*Skew(w)/norm(w)+(1-cos(norm(w)))*Skew(w)*Skew(w)/(norm(w)^2);
     end
 end
